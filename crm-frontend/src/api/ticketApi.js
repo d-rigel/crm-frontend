35d89a9a6e0dc575a42fe65ea3d4330c87dbb171
d-rigel <alozie4God@gmail.com>
2021-10-15 00:23:44 -0700

    get all ticket api integrated

--- crm-frontend/src/api/ticketApi.js
+++ crm-frontend/src/api/ticketApi.js
@@ -5,8 +5,7 @@ export const getAlltickets = async () => {
     try {
       const result = await axios.get("http://localhost:5000/v1/ticket", {
         headers: {
-          Authorization:
-            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InJpZ2VsNGdAZ21haWwuY29tIiwiaWF0IjoxNjM0MjM5Mjg3LCJleHAiOjE2MzQyNDAxODd9.Uhg60wNXz1mSNZ6GA3CiEMFzIrx9DHaQbxMFfjUw8ug",
+          Authorization: sessionStorage.getItem("accessToken"),
         },
       });
       resolve(result);
